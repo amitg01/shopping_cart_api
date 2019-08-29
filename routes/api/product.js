52c85b56e1f14b019dc113eff1ced855bddb5e7a
amit gupta <amitg.ghy@gmail.com>
2019-08-29 21:43:57 +0530

    get single product

--- routes/api/product.js
+++ routes/api/product.js
@@ -25,15 +25,20 @@ router.post("/", (req, res) => {
 
 router.put("/update/:id", (req, res) => {
   var id = req.params.id;
-  Product.findByIdAndUpdate(id,req.body, { upsert: true,new: true }, (err, product) => {
-    if (err)
+  Product.findByIdAndUpdate(
+    id,
+    req.body,
+    { upsert: true, new: true },
+    (err, product) => {
+      if (err)
+        return res
+          .status(400)
+          .json({ success: false, msg: "failed to update product" });
       return res
-        .status(400)
-        .json({ success: false, msg: "failed to update product" });
-    return res
-      .status(200)
-      .json({ success: true, msg: "product updated", product });
-  });
+        .status(200)
+        .json({ success: true, msg: "product updated", product });
+    }
+  );
 });
 
 // get all products
@@ -42,6 +47,18 @@ router.get("/", (req, res) => {
   Product.find((err, products) => {
     if (err) return res.status(404).json({ success: false, err });
     return res.status(200).json({ success: true, products });
+  });
+});
+
+// get a single product
+
+router.get("/:id", (req, res) => {
+  Product.findOne({_id:req.params.id}, (err, product) => {
+    if (err)
+      return res
+        .status(404)
+        .json({ success: true, msg: "product not found", err });
+    return res.status(400).json({ success: true, product });
   });
 });
 
