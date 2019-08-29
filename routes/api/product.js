4ee70029db399ed047f6bb0f9bc4d321262bf17e
amit gupta <amitg.ghy@gmail.com>
2019-08-29 22:00:11 +0530

    delete single product

--- routes/api/product.js
+++ routes/api/product.js
@@ -53,12 +53,24 @@ router.get("/", (req, res) => {
 // get a single product
 
 router.get("/:id", (req, res) => {
-  Product.findOne({_id:req.params.id}, (err, product) => {
+  Product.findOne({ _id: req.params.id }, (err, product) => {
     if (err)
       return res
         .status(404)
         .json({ success: true, msg: "product not found", err });
-    return res.status(400).json({ success: true, product });
+    return res.status(200).json({ success: true, product });
+  });
+});
+
+// delete an item
+
+router.delete("/delete/:id", (req, res) => {
+  Product.findOneAndDelete({ _id: req.params.id }, (err) => {
+    if (err)
+      return res
+        .status(400)
+        .json({ success: false, msg: "failed to delete product", err });
+    return res.status(200).json({ success: true, msg: "product deleted" });
   });
 });
 
